916996b1bcdcb4966adcc5327de742313511d709
timtimjnvr <timotheejanvier01@gmail.com>
2021-04-21 09:19:25 +0200

    get request and find user in the database

--- Backend/Api/User.js
+++ Backend/Api/User.js
@@ -3,13 +3,25 @@ const mongooose = require('mongoose');
 const User = require('../DB/User');
 const route = express.Router();
 
-route.get('/', (req,res) => {
-    res.send('API is working properly');
+route.get('/:pseudo', (req,res) => {
+    const pseudo = req.params.pseudo;
+    User.findOne({mail: pseudo})
+    .exec()
+    .then(doc => {
+        console.log(doc);
+        console.log('Utilisateur trouvé')
+        res.status(200).json(doc);
+    })
+    .catch((err) => {
+        console.log(err);
+        res.status(500).json({error: err})
+    })
 });
 
 route.post('/', async (req,res) => {
     const {pseudo,mail,password} = req.body;
     let user = {};
+    user._id = new mongooose.Types.ObjectId();
     user.pseudo = pseudo;
     user.mail = mail;
     user.password = password;
--- Backend/server.js
+++ Backend/server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const connectDB = require('./DB/connection.js');
 const app = express();
 
-connectDB();
+connectDB(); //connect
 
 app.use(express.json({extended: false}));
+
 app.use('/api/userModel', require('./Api/User'));
+
 const port = process.env.port || 9000;
 
 app.listen(port, () => console.log('Server Started'));
